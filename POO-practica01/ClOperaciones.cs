7d24dd67ab2fcf8a8e2e588dfffad6cf1c2ebfc5
Hector Joel Velarde Puga <hjoel5511@hotmail.com>
2024-08-28 08:39:38 -0700

    Agregar archivos de proyecto.

---
+++ POO-practica01/ClOperaciones.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_practica01
+{
+    internal class ClOperaciones
+    {
+        //Cómo es (caracteristicas, campos)
+        private decimal val1;
+        private decimal val2;
+
+        public ClOperaciones(decimal val1, decimal val2)
+        {
+            this.val1 = val1;
+            this.val2 = val2;
+        }
+
+        //Propiedades get, set
+
+        //Constructores
+
+        //Lo que hace (acción)
+        public decimal suma()
+        {
+            return ( val1 + val2 );
+        }
+
+        public decimal resta()
+        {
+            return ( val1 - val2 );
+        }
+        public decimal division()
+        {
+            return (val1 - val2);
+        }
+        public decimal multi()
+        {
+            return (val1 - val2);
+        }
+
+    }
+}
---
+++ POO-practica01/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace POO_practica01
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSuma_Click(object sender, RoutedEventArgs e)
+        {
+            ClOperaciones op = new ClOperaciones(decimal.Parse(TextBoxValor1.Text), decimal.Parse(TextBoxValor2.Text));
+            TxtBoxResult.Text = op.suma().ToString();
+        }
+
+        private void BtnResta_Click(object sender, RoutedEventArgs e)
+        {
+            ClOperaciones op = new ClOperaciones(decimal.Parse(TextBoxValor1.Text), decimal.Parse(TextBoxValor2.Text));
+            TxtBoxResult.Text = op.resta().ToString();
+        }
+
+        private void BtnMulti_Click(object sender, RoutedEventArgs e)
+        {
+            ClOperaciones op = new ClOperaciones(decimal.Parse(TextBoxValor1.Text), decimal.Parse(TextBoxValor2.Text));
+            TxtBoxResult.Text = op.multi().ToString();
+        }
+
+        private void BtnDivision_Click(object sender, RoutedEventArgs e)
+        {
+            ClOperaciones op = new ClOperaciones(decimal.Parse(TextBoxValor1.Text), decimal.Parse(TextBoxValor2.Text));
+            TxtBoxResult.Text = op.division().ToString();
+        }
+    }
+}
